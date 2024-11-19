b78c56f2b1fc7fe639481c9e60d66e4bed40e6d8
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-11-19 13:41:35 -0400

    reset cover animation on song switch

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 const Song = ({ currentSong, isPlaying }) => {
+  useEffect(() => {
+    const resetAnimation = () => {
+      // resets cover to its original position
+      const el = document.querySelector(".rotateSong");
+      if (el && el.style) {
+        el.style.animation = "none";
+        void el.offsetWidth;
+        el.style.animation = "";
+      }
+    };
+    resetAnimation();
+  }, [currentSong]);
+
   return (
     <div className="song-container">
-      <img
-        alt="album-cover"
-        className={isPlaying ? 'rotateSong' : ''}
-        src={currentSong.cover}
-      />
+      <img alt="album-cover" className={isPlaying ? "rotateSong" : ""} src={currentSong.cover} />
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
     </div>
